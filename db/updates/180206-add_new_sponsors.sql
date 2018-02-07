bbdbca596a0c50e58170953dd39cec6655a6bef0
bufutda <s@watz.ky>
2018-02-06 20:11:37 -0700

    Add more sponsors

---
+++ db/updates/180206-add_new_sponsors.sql
@@ -0,0 +1,5 @@
+USE `asec`;
+
+INSERT INTO `sponsors` VALUES
+    (UUID(), "Table", "Sproule", "sproule.png", "https://sproule.com/", 2018),
+    (UUID(), "Table", "Canadian Association of Petroleum Producers", "capp.png", "https://www.capp.ca/", 2018);
